ac7fe4466fdf5743fc4d59ef206a1aaab3009e1f
Ethan Hoover <51389138+Cireimu@users.noreply.github.com>
2019-10-29 21:14:33 -0700

    mvp?

--- react-github-card/src/App.js
+++ react-github-card/src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
+import UserCard from './components/userCard';
+import AddUserForm from './components/addUserForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super()
+    this.state = {
+        username: 'cireimu',
+        usersList: []
+    }
+  }
+
+  componentDidMount(){
+    this.fetchUsers()
+
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    if (prevState.username !== this.state.username){
+      console.log(this.state.usersList)
+      this.fetchUsers()
+    }
+  }
+
+
+  fetchUsers = (user = `${this.state.username}`) => {
+    axios.get(`https://api.github.com/users/${user}`)
+    .then(resp=>{
+      console.log(resp)
+      this.setState({
+      usersList: [...this.state.usersList, resp.data]
+      })
+    })
+    .catch(err =>{
+      console.log('error: ', err)
+    })
+
+  }
+
+  addUser = (event, user) => {
+    const newUser = user
+    this.setState({
+      username: newUser
+    })
+  }
+
+ 
+
+  render(){
+    return (
+      <div className="App">
+        <h1> GitHub User Cards </h1>
+        <AddUserForm addUser={this.addUser}/>
+        <div className="users">
+          {this.state.usersList.map((item, index)=>{
+            return(
+              <div className="user-card" key={index}>
+                <UserCard detail ={item} />
+              </div>
+              )
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
